3ca023f0916dab60b3877c2cd752f818bf78aedd
fshaaa <fathshaffa@gmail.com>
2022-09-20 13:11:16 +0700

    fix: fix select.sql prak sec 13

--- 13_Join – Union – Agregasi – Subquery – Function (DBMS)/praktikum/SELECT.sql
+++ 13_Join – Union – Agregasi – Subquery – Function (DBMS)/praktikum/SELECT.sql
@@ -6,4 +6,13 @@ SELECT name FROM products WHERE product_id = 3;
 
 -- tampilkan pelanggan yang created at kurang dalam seminggu 
 -- dan mempunyai nama yang terdapat 'a'
-SELECT name FROM users WHERE created_at > (DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AND name LIKE "%a%"
+SELECT name FROM users WHERE created_at > (DATE_SUB(CURDATE(), INTERVAL 7 DAY)) AND name LIKE "%a%";
+
+-- hitung jumlah user dengan status gender perempuan
+SELECT COUNT(user_id) FROM users WHERE gender = 'F';
+
+-- tampilkan data pelanggan dengan nama urut abjad
+SELECT * FROM users ORDER BY name ASC;
+
+-- tampilkan 5 data product
+SELECT * FROM products LIMIT 5;
